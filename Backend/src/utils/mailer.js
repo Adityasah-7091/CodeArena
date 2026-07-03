db675c80b00dac5cf1757f75856c88bdb92eb0fa
Adityasah-7091 <adityasah0620@gamil.com>
2026-07-03 11:45:40 +0530

    fix: force ipv4 in mailer transporter to resolve render connectivity issue

--- Backend/src/utils/mailer.js
+++ Backend/src/utils/mailer.js
@@ -8,7 +8,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4 // Force IPv4 to prevent IPv6 network connection issues on Render
 });
 
 /**
